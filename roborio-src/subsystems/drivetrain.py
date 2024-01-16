668553f8cd262445508c09ba2d63cc1aa279a0cf
Brian Jarrett <celttechie@gmail.com>
2024-01-16 07:33:23 -0600

    Fixed imports, added SwerveChassis attributes for init

--- roborio-src/subsystems/drivetrain.py
+++ roborio-src/subsystems/drivetrain.py
@@ -1,8 +1,21 @@
 # High level objects that control our drivetrain
-from FROGlib.swerve import SwerveChassis
+from FROGlib.swerve import SwerveChassis, SwerveModule
 from commands2.subsystem import Subsystem
+from constants import kSwerveDriveGearing
+from FROGlib.sensors import FROGGyro
+from constants import kMaxChassisRadiansPerSec, kMaxChassisRadiansPerSec
 
 class DriveTrain(SwerveChassis, Subsystem):
     def __init__(self):
-        SwerveChassis.__init__()
+        SwerveChassis.__init__(
+            modules=(
+                SwerveModule('FrontLeft'),
+                SwerveModule('FrontRight'),
+                SwerveModule('BackLeft'),
+                SwerveModule('BackRight')
+            ),
+            gyro=FROGGyro(),
+            max_speed=self.max_speed,
+            max_rotation_speed = kMaxChassisRadiansPerSec
+        )
         Subsystem.__init__()
