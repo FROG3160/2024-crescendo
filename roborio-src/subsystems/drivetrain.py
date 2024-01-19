a741b0a56e710977bea77a5eee75f9e5078d0138
Joseph Wang <joseph007wang@gmail.com>
2024-01-18 19:59:50 -0600

    Added AutoBuilder Code Structure, added two of four necessary methods.

--- roborio-src/subsystems/drivetrain.py
+++ roborio-src/subsystems/drivetrain.py
@@ -3,6 +3,10 @@ from FROGlib.swerve import SwerveChassis, SwerveModule
 from FROGlib.sensors import FROGGyro
 from constants import kMaxChassisRadiansPerSec, kMaxMetersPerSecond
 import configs
+# Objects needed for Auto setup (AutoBuilder)
+from pathplannerlib.auto import AutoBuilder
+from pathplannerlib.config import HolonomicPathFollowerConfig, ReplanningConfig, PIDConstants
+from wpilib import DriverStation
 
 class DriveTrain(SwerveChassis):
     def __init__(self):
@@ -16,4 +20,20 @@ class DriveTrain(SwerveChassis):
             gyro=FROGGyro(),
             max_speed=kMaxMetersPerSecond,
             max_rotation_speed = kMaxChassisRadiansPerSec
+        )
+        # Configure the AutoBuilder last
+        AutoBuilder.configureHolonomic(
+            self.getPose, # Robot pose supplier
+            self.resetPose, # Method to reset odometry (will be called if your auto has a starting pose)
+            self.getRobotRelativeSpeeds, # ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            self.robotOrientedDrive, # Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            HolonomicPathFollowerConfig( # HolonomicPathFollowerConfig, this should likely live in your Constants class
+                PIDConstants(5.0, 0.0, 0.0), # Translation PID constants
+                PIDConstants(5.0, 0.0, 0.0), # Rotation PID constants
+                4.5, # Max module speed, in m/s
+                0.4, # Drive base radius in meters. Distance from robot center to furthest module.
+                ReplanningConfig() # Default path replanning config. See the API for the options here
+            ),
+            self.shouldFlipPath, # Supplier to control path flipping based on alliance color
+            self # Reference to this subsystem to set requirements
         )
