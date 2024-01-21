d831dbcf85876964a77f0c784b27a953723d821c
Joseph Wang <joseph007wang@gmail.com>
2024-01-20 21:24:09 -0600

    Replace filler steer PID constants with the calculated ones Fixes #37

--- roborio-src/subsystems/drivetrain.py
+++ roborio-src/subsystems/drivetrain.py
@@ -1,7 +1,7 @@
 # High level objects that control our drivetrain
 from FROGlib.swerve import SwerveChassis, SwerveModule
 from FROGlib.sensors import FROGGyro
-from constants import kMaxChassisRadiansPerSec, kMaxMetersPerSecond, kDriveBaseRadius
+from constants import kMaxChassisRadiansPerSec, kMaxMetersPerSecond, kDriveBaseRadius, kSteerP, kSteerI
 import configs
 # Objects needed for Auto setup (AutoBuilder)
 from pathplannerlib.auto import AutoBuilder
@@ -29,7 +29,8 @@ class DriveTrain(SwerveChassis):
             self.robotOrientedDrive, # Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
             HolonomicPathFollowerConfig( # HolonomicPathFollowerConfig, this should likely live in your Constants class
                 PIDConstants(0.0, 0.0, 0.0), # Translation PID constants
-                PIDConstants(5.0, 0.0, 0.0), # Rotation PID constants
+                #ISSUE #37 Replace filler steer PID constants with the calculated ones
+                PIDConstants(kSteerP, kSteerI, 0.0), # Rotation PID constants
                 kMaxMetersPerSecond, # Max module speed, in m/s
                 kDriveBaseRadius, # Drive base radius in meters. Distance from robot center to furthest module.
                 ReplanningConfig() # Default path replanning config. See the API for the options here
