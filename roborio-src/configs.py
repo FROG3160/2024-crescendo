d477279a5db7c80c079d811e3ec8f8220fbb2d7b
Brian Jarrett <celttechie@gmail.com>
2024-01-23 05:58:01 -0600

    fixed incomplete import line

--- roborio-src/configs.py
+++ roborio-src/configs.py
@@ -6,7 +6,6 @@ from FROGlib.sensors import FROGCANCoderConfig
 from phoenix6.configs.config_groups import Slot0Configs, Slot1Configs
 from phoenix6.signals.spn_enums import FeedbackSensorSourceValue
 from pathplannerlib.config import PIDConstants
-from wpilib.
 
 steerGains = Slot0Configs().with_k_p(constants.kSteerP).with_k_i(constants.kSteerI)
 driveDutyCycleGains = Slot0Configs().with_k_s(constants.kDutyCycleDriveS).with_k_v(constants.kDutyCycleDriveV)
@@ -108,4 +107,4 @@ swerveModuleBackRight = {
     'cancoder_config':FROGCANCoderConfig(constants.kBackRightOffset)
 }
 
-robotToLimelightTransform = Transform3d(Translation3d(0.5, 0.0, 0.5), Rotation3d(0,0,math.pi));
+robotToLimeLightTransform = Transform3d(Translation3d(0.5, 0.0, 0.5), Rotation3d(0,0,math.pi));
