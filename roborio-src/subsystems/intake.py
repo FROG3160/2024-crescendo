b3aa8566ea9b643b927b7025163a65c003ac36e2
Brian Jarrett <celttechie@gmail.com>
2024-01-22 19:32:25 -0600

    Added intake.py with basic notes and structure.

---
+++ roborio-src/subsystems/intake.py
@@ -0,0 +1,7 @@
+#The intake consists of a roller bar (TalonSRX), an intake wheel(SparkMax), and a transfer wheel(SparkMax
+from rev import CANSparkMax
+from commands2 import Subsystem
+
+class Intake(Subsystem):
+    def __init__(self):
+        pass
