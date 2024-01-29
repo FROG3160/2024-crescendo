9568a1033a9fa9c930403a92319d96ccaf9a947f
Joseph Wang <joseph007wang@gmail.com>
2024-01-28 20:47:14 -0600

    Accidentally edited main for issue #52.  Committing current changes.

--- roborio-src/subsystems/intake.py
+++ roborio-src/subsystems/intake.py
@@ -7,7 +7,7 @@ class Intake(Subsystem):
     
     def __init__(self):
         
-        self.intakeWheel = CANSparkMax(kIntakeWheelsControllerID, CANSparkMax.MotorType.kBrushless)
+        self.intakeWheel = CANSparkMax(kIntakeRollerControllerID, CANSparkMax.MotorType.kBrushless)
         self.transferWheel = CANSparkMax(kTransferWheelsID, CANSparkMax.MotorType.kBrushless)
 
 
