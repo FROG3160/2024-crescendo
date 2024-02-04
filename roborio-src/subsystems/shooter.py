060dac9e279394c9699fc03d9726f8591e183c89
Brian Jarrett <celttechie@gmail.com>
2024-02-04 17:57:05 -0600

    refactored flywheel methods, applied formatting

--- roborio-src/subsystems/shooter.py
+++ roborio-src/subsystems/shooter.py
@@ -1,9 +1,16 @@
 from commands2 import Subsystem
 from FROGlib.motors import FROGTalonFX, FROGTalonFXConfig
-from phoenix6.controls import PositionDutyCycle, VelocityDutyCycle, VelocityVoltage, PositionVoltage
+from phoenix6.controls import (
+    PositionDutyCycle,
+    VelocityDutyCycle,
+    VelocityVoltage,
+    PositionVoltage,
+    VoltageOut,
+)
 from rev import CANSparkMax
 import constants
 import configs
+
 
 class Shooter(Subsystem):
 
@@ -19,42 +26,39 @@ class Shooter(Subsystem):
         right_flywheel_id: int,
         right_flywheel_config: FROGTalonFXConfig,
         sequencer_id: int,
-        sequencer_motor_type
+        sequencer_motor_type,
     ):
-        
-        # Very rudimentary system that allows speed control  of 
+
+        # Very rudimentary system that allows speed control  of
         # the lead screw and flywheel on the operator Xbox controller triggers.
         # The NEO 550 will be used to pull and push the note out of
         # the intake and into the flywheel.
-        
+
         self.leadScrew = FROGTalonFX(lead_screw_id, lead_screw_config)
         self.leftFlyWheel = FROGTalonFX(left_flywheel_id, left_flywheel_config)
         self.rightFlyWheel = FROGTalonFX(right_flywheel_id, right_flywheel_config)
         self.sequencer = CANSparkMax(sequencer_id, sequencer_motor_type)
-                  
-    def getFlywheelSpeed(self, flywheelSpeed: float):
+
+    def setFlywheelSpeed(self, flywheelSpeed: float):
         self.flywheelSpeed = flywheelSpeed
-        
-    def setFlywheelSpeeds(self):
+
+    def runFlywheels(self):
         self.leftFlyWheel.set_control(
-            VelocityVoltage(
-                velocity=self.flywheelSpeed,
-                slot=0
-            )
+            VelocityVoltage(velocity=self.flywheelSpeed, slot=0)
         )
         self.rightFlyWheel.set_control(
-            VelocityVoltage(
-                velocity=-self.flywheelSpeed,
-                slot=0
-            )
+            VelocityVoltage(velocity=-self.flywheelSpeed, slot=0)
         )
 
     def getFlyWheelSpeedsIsTrue(self) -> bool:
-        if self.flywheelSpeed == self.leftFlyWheel.get_velocity() and self.flywheelSpeed == abs(self.rightFlyWheel.get_velocity()):
+        if (
+            self.flywheelSpeed == self.leftFlyWheel.get_velocity()
+            and self.flywheelSpeed == abs(self.rightFlyWheel.get_velocity())
+        ):
             return True
         else:
             return False
-        
+
     def runSequencer(self):
         self.sequencer.set(constants.kSequencerSpeed)
 
@@ -70,8 +74,3 @@ class Shooter(Subsystem):
             return True
         else:
             return False
-
-
-
-
-
