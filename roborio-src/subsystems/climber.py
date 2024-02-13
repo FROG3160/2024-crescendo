ba2a49b4841b6ffc61541660a60093e29790304c
Joseph Wang <joseph007wang@gmail.com>
2024-02-13 17:23:53 -0600

    Created climber.py

---
+++ roborio-src/subsystems/climber.py
@@ -0,0 +1,36 @@
+from commands2 import Subsystem
+from FROGlib.motors import FROGTalonFX, FROGTalonFXConfig
+from phoenix6.controls import (
+    PositionDutyCycle,
+    VelocityDutyCycle,
+    VelocityVoltage,
+    PositionVoltage,
+    VoltageOut,
+    MotionMagicVoltage
+)
+
+class Climber(Subsystem):
+    
+    def __init__(
+        self,
+        left_climber_id: int,
+        left_climber_config: FROGTalonFXConfig,
+        right_climber_id: int,
+        right_climber_config: FROGTalonFXConfig
+    ):
+
+        self.leftClimber = FROGTalonFX(left_climber_id, left_climber_config)
+        self.rightClimber = FROGTalonFX(right_climber_id, right_climber_config)
+
+    def climberVoltage(self, climberVoltage: float):
+        self.climberVoltage = climberVoltage
+
+    def engageClimber(self):
+        self.leftClimber.set_control(
+            VoltageOut(volt=self.climberVoltage)
+        )
+        self.rightClimber.set_control(
+            VoltageOut(volt=-self.climberVoltage)
+        )
+
+    
