44dc991167abc7331680c4566b9faa29febc70ed
Brian Jarrett <celttechie@gmail.com>
2024-02-22 09:41:01 -0600

    renamed vision subsystems

--- roborio-src/subsystems/vision.py
+++ roborio-src/subsystems/vision.py
@@ -3,50 +3,35 @@ from wpimath.geometry import Pose3d
 from photonlibpy.photonCamera import PhotonCamera
 from photonlibpy.photonPoseEstimator import PhotonPoseEstimator, PoseStrategy
 from photonlibpy.photonTrackedTarget import PhotonTrackedTarget
-from FROGlib.limelight import FROGLLField
+from FROGlib.limelight import FROGPositioning, FROGTargeting
 from robotpy_apriltag import loadAprilTagLayoutField, AprilTagField
 from configs import robotToLimeLightTransform
 from constants import kPhotonCameraName
 from wpilib import SmartDashboard
 
 
-class VisionSubsystem(Subsystem):
+class PositioningSubsystem(Subsystem):
     def __init__(self):
         super().__init__()
-        self.estimator = PhotonPoseEstimator(
-            loadAprilTagLayoutField(AprilTagField.k2024Crescendo),
-            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-            PhotonCamera(kPhotonCameraName),
-            robotToLimeLightTransform,
-        )
-
-    def periodic(self) -> None:
-        self.latestPose = self.estimator.update()
-        if self.latestPose:
-            SmartDashboard.putString('Vision Estimate', self.latestPose.estimatedPose.__str__())
-
-    def getLatestPoseEstimate(self) -> tuple[Pose3d, float]:
-        if self.latestPose:
-            return (self.latestPose.estimatedPose, self.latestPose.timestampSeconds)
-
-    def getTargetsUsed(self) -> list[PhotonTrackedTarget]:
-        if self.latestPose:
-            return self.latestPose.targetsUsed
-        
-
-class AprilTagSubsystem(Subsystem):
-    def __init__(self):
-        super().__init__()
-        self.estimator = FROGLLField('limelight-field')
+        self.estimator = FROGPositioning("limelight-field")
 
     def periodic(self) -> None:
         self.latestPose = self.estimator.getBotPoseEstimateBlue()
         if self.latestPose:
-            SmartDashboard.putString('Vision Estimate', self.latestPose[0].__str__())
+            SmartDashboard.putString("Vision Estimate", self.latestPose[0].__str__())
         self.latestTransform = self.estimator.getTargetTransform()
         if self.latestTransform:
-            SmartDashboard.putString('Target Transform', self.latestTransform.__str__())
-    
+            SmartDashboard.putString("Target Transform", self.latestTransform.__str__())
+
     def getLatestPoseEstimate(self) -> tuple[Pose3d, float]:
         if self.latestPose:
             return (self.latestPose[0], self.latestPose[1])
+
+
+class TargetingSubsystem(Subsystem):
+    def __init__(self):
+        super().__init__()
+        self.camera = FROGTargeting
+
+    def periodic(self) -> None:
+        self.target = self.camera.getTarget()
