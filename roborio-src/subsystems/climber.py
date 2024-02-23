4dd58fb8965be3a9c6cfcc077ed241454adf597a
Brian Jarrett <celttechie@gmail.com>
2024-02-23 11:01:07 -0600

    Added nt instance and basic telemetry for climber

--- roborio-src/subsystems/climber.py
+++ roborio-src/subsystems/climber.py
@@ -6,29 +6,41 @@ from phoenix6.controls import (
     VelocityVoltage,
     PositionVoltage,
     VoltageOut,
-    MotionMagicVoltage
+    MotionMagicVoltage,
 )
+from ntcore import NetworkTableInstance
 
-class Climber(Subsystem):
-    
+
+class ClimberSubsystem(Subsystem):
+
     def __init__(
         self,
         left_climber_id: int,
         left_climber_config: FROGTalonFXConfig,
         right_climber_id: int,
-        right_climber_config: FROGTalonFXConfig
+        right_climber_config: FROGTalonFXConfig,
+        parent_nt: str = "Subsystems",
     ):
-
+        super().__init__()
+        self.setName("Climber")
         self.leftClimber = FROGTalonFX(left_climber_id, left_climber_config)
         self.rightClimber = FROGTalonFX(right_climber_id, right_climber_config)
+        nt_table = f"{parent_nt}/{self.getName()}"
+        self.motorCommandedSpeed = 0
+
+        self._motorCommandedSpeedPub = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/motor/speed")
+            .publish()
+        )
 
     def setVoltage(self, climberVoltage: float):
         self.climberVoltage = climberVoltage
-        self.leftClimber.set_control(
-            VoltageOut(volt=self.climberVoltage)
-        )
-        self.rightClimber.set_control(
-            VoltageOut(volt=-self.climberVoltage)
-        )
+        self.leftClimber.set_control(VoltageOut(volt=self.climberVoltage))
+        self.rightClimber.set_control(VoltageOut(volt=-self.climberVoltage))
 
-    
+    def periodic(self) -> None:
+        self.logTelemetry()
+
+    def logTelemetry(self):
+        self._motorCommandedSpeedPub.set(self.motorCommandedSpeed)
