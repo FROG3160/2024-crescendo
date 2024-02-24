96cdcd602c52cf168ab68a56b9c814c5650631b0
Brian Jarrett <celttechie@gmail.com>
2024-02-24 10:21:59 -0600

    Added motor configs, telemetry, instantiated

--- roborio-src/subsystems/climber.py
+++ roborio-src/subsystems/climber.py
@@ -1,4 +1,4 @@
-from commands2 import Subsystem
+from commands2 import Command, Subsystem
 from FROGlib.motors import FROGTalonFX, FROGTalonFXConfig
 from phoenix6.controls import (
     PositionDutyCycle,
@@ -9,38 +9,80 @@ from phoenix6.controls import (
     MotionMagicVoltage,
 )
 from ntcore import NetworkTableInstance
+from constants import kLeftClimberControllerID, kRightClimberControllerID
+from configs import leftClimberMotorConfig, rightClimberMotorConfig
 
 
 class ClimberSubsystem(Subsystem):
 
     def __init__(
         self,
-        left_climber_id: int,
-        left_climber_config: FROGTalonFXConfig,
-        right_climber_id: int,
-        right_climber_config: FROGTalonFXConfig,
         parent_nt: str = "Subsystems",
     ):
         super().__init__()
         self.setName("Climber")
-        self.leftClimber = FROGTalonFX(left_climber_id, left_climber_config)
-        self.rightClimber = FROGTalonFX(right_climber_id, right_climber_config)
+        self.leftClimber = FROGTalonFX(kLeftClimberControllerID, leftClimberMotorConfig)
+        self.rightClimber = FROGTalonFX(
+            kRightClimberControllerID, rightClimberMotorConfig
+        )
         nt_table = f"{parent_nt}/{self.getName()}"
-        self.motorCommandedSpeed = 0
+        self.motorCommandValue = 0
+        self.motorControlType = ""
 
-        self._motorCommandedSpeedPub = (
+        self._motorCommandedValuePub = (
             NetworkTableInstance.getDefault()
-            .getFloatTopic(f"{nt_table}/motor/speed")
+            .getFloatTopic(f"{nt_table}/command_value")
+            .publish()
+        )
+        self._motorControlTypePub = (
+            NetworkTableInstance.getDefault()
+            .getStringTopic(f"{nt_table}/command_type")
+            .publish()
+        )
+        self._leftMotorTorque = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/torque/left")
+            .publish()
+        )
+        self._rightMotorTorque = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/torque/right")
             .publish()
         )
 
+    def getTorque(self, motor: FROGTalonFX) -> float:
+        return motor.get_torque_current().value
+
     def setVoltage(self, climberVoltage: float):
-        self.climberVoltage = climberVoltage
-        self.leftClimber.set_control(VoltageOut(volt=self.climberVoltage))
-        self.rightClimber.set_control(VoltageOut(volt=-self.climberVoltage))
+        motorControl = VoltageOut(climberVoltage)
+        self.motorCommandedValue = motorControl.output
+        self.motorControlType = motorControl.name()
+        self.leftClimber.set_control(motorControl)
+        self.rightClimber.set_control(motorControl)
+
+    def extend(self):
+        self.setVoltage(-3)
+
+    def retract(self):
+        self.setVoltage(3)
+
+    def retractSlow(self):
+        self.setVoltage(0.5)
+
+    def stop(self):
+        self.setVoltage(0)
 
     def periodic(self) -> None:
         self.logTelemetry()
 
+    def get_ExtendCommand(self) -> Command:
+        return self.startEnd(self.extend, self.stop)
+
+    def get_RetractCommand(self) -> Command:
+        return self.startEnd(self.retract, self.stop)
+
     def logTelemetry(self):
-        self._motorCommandedSpeedPub.set(self.motorCommandedSpeed)
+        self._motorCommandedValuePub.set(self.motorCommandValue)
+        self._motorControlTypePub.set(self.motorControlType)
+        self._leftMotorTorque.set(self.getTorque(self.leftClimber))
+        self._rightMotorTorque.set(self.getTorque(self.rightClimber))
