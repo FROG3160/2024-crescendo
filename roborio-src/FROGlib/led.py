8bb3e0098183f950e8bb464286b96309d30ea51b
Brian Jarrett <celttechie@gmail.com>
2024-03-24 16:04:10 -0500

    Fixed issue with colorflowanimation constants.

--- roborio-src/FROGlib/led.py
+++ roborio-src/FROGlib/led.py
@@ -1,6 +1,7 @@
-from led import CANdle
-from led import LEDStripType
-from led import (
+from commands2 import Subsystem
+from phoenix5.led import CANdle
+from phoenix5.led import LEDStripType
+from phoenix5.led import (
     RainbowAnimation,
     TwinkleAnimation,
     ColorFlowAnimation,
@@ -8,25 +9,31 @@ from led import (
     SingleFadeAnimation,
     StrobeAnimation,
     BaseTwoSizeAnimation,
-    LarsonAnimation
+    LarsonAnimation,
 )
 
 BRIGHTNESS = 0.4
-FORWARD = ColorFlowAnimation.FORWARD
-BACKWARD = ColorFlowAnimation.BACKWARD
+FORWARD = ColorFlowAnimation.Direction.Forward
+BACKWARD = ColorFlowAnimation.Direction.Backward
 NUM_CANDLE_LEDS = 8
 NUM_STRIP_LEDS = 29
-NUM_TOTAL_LEDS = NUM_CANDLE_LEDS + NUM_STRIP_LEDS  # the 8 LEDs of the CANdle + the strip
+NUM_TOTAL_LEDS = (
+    NUM_CANDLE_LEDS + NUM_STRIP_LEDS
+)  # the 8 LEDs of the CANdle + the strip
 RAINBOW = RainbowAnimation(1, 0.9, NUM_TOTAL_LEDS)
 TWINKLE = TwinkleAnimation(0, 225, 0, 225, 0.5, NUM_TOTAL_LEDS)
 COLORFLOWFORWARD = ColorFlowAnimation(125, 235, 0, 0, 0.5, NUM_TOTAL_LEDS, FORWARD)
 COLORFLOWBACKWARD = ColorFlowAnimation(125, 235, 3, 0, 0.5, NUM_TOTAL_LEDS, BACKWARD)
-REDALLIANCE = ColorFlowAnimation(64, 0, 0, 0, 0.05, NUM_STRIP_LEDS, FORWARD, NUM_CANDLE_LEDS)
-BLUEALLIANCE = ColorFlowAnimation(0, 0, 64, 0, 0.05, NUM_STRIP_LEDS, FORWARD, NUM_CANDLE_LEDS)
+REDALLIANCE = ColorFlowAnimation(
+    64, 0, 0, 0, 0.05, NUM_STRIP_LEDS, FORWARD, NUM_CANDLE_LEDS
+)
+BLUEALLIANCE = ColorFlowAnimation(
+    0, 0, 64, 0, 0.05, NUM_STRIP_LEDS, FORWARD, NUM_CANDLE_LEDS
+)
 FIRE = FireAnimation(1, 0.5, NUM_TOTAL_LEDS + 15, 0.7, 0.3, False, NUM_CANDLE_LEDS)
 
 
-class FROGLED:
+class FROGLED(Subsystem):
     def __init__(self, canID):
         self.candle = CANdle(canID)
         self.candle.configLEDType(LEDStripType.GRB)
@@ -36,8 +43,16 @@ class FROGLED:
     def larsonAnimation(self, r, g, b, speed):
         self.candle.animate(
             LarsonAnimation(
-                r, g, b, 0, speed, NUM_STRIP_LEDS, LarsonAnimation.BounceMode.Front, 7, NUM_CANDLE_LEDS
-            )   
+                r,
+                g,
+                b,
+                0,
+                speed,
+                NUM_STRIP_LEDS,
+                LarsonAnimation.BounceMode.Front,
+                7,
+                NUM_CANDLE_LEDS,
+            )
         )
 
     def default(self):
@@ -53,24 +68,16 @@ class FROGLED:
         self.candle.animate(BLUEALLIANCE)
 
     def yellowPocketSlow(self):
-        self.larsonAnimation(
-                250, 129, 7, 0.5
-        )
+        self.larsonAnimation(250, 129, 7, 0.5)
 
     def yellowPocketFast(self):
-        self.larsonAnimation(
-                250, 129, 7, 1.0
-        )
+        self.larsonAnimation(250, 129, 7, 1.0)
 
     def purplePocketSlow(self):
-        self.larsonAnimation(
-                167, 16, 201, 0.5
-        )
+        self.larsonAnimation(167, 16, 201, 0.5)
 
     def purplePocketFast(self):
-        self.larsonAnimation(
-                167, 16, 201, 1.0
-        )
+        self.larsonAnimation(167, 16, 201, 1.0)
 
     def purple(self):
         self.candle.setLEDs(167, 16, 201)
@@ -99,5 +106,6 @@ class FROGLED:
     def lightPink(self):
         self.candle.setLEDs(255, 153, 255)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
